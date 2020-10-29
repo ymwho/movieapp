88cb11254c973b00e5bb802690d115030ffa0af9
yeongmin <parametertree@gmail.com>
2020-10-29 16:02:23 +0900

    Updates

---
+++ precache-manifest.68ac488e328822c1ebf500abb56b8d9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c864161ade8a198d33147c414f6e84d",
+    "url": "/movieapp/index.html"
+  },
+  {
+    "revision": "ccd93813378cb2f6473c",
+    "url": "/movieapp/static/css/main.718ba31b.chunk.css"
+  },
+  {
+    "revision": "7533e941d472ff27aa6a",
+    "url": "/movieapp/static/js/2.88fd0bd4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movieapp/static/js/2.88fd0bd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccd93813378cb2f6473c",
+    "url": "/movieapp/static/js/main.36f86403.chunk.js"
+  },
+  {
+    "revision": "b1442b2c399c95193047",
+    "url": "/movieapp/static/js/runtime-main.de5613db.js"
+  }
+]);
